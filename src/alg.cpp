d5ded26d888f1f16f3347aef06083da6de6b8eee
MariaFokina <78910429+MariaFokina@users.noreply.github.com>
2021-03-22 14:34:07 +0300

    Create alg.cpp

---
+++ src/alg.cpp
@@ -0,0 +1,22 @@
+// Copyright 2021 NNTU-CS
+
+int cbinsearch(int *arr, int size, int value) {
+  int right = 0, left = size - 1, sum = 0;
+  while (right < left) {
+    int mid = (right + left) / 2;//
+    if (arr[mid] < value)//
+      right = mid + 1;
+    else
+      left = mid;
+  }
+  if (arr[right] == value) {
+    while (arr[right] == value) {
+      sum++;
+      right++;
+    }
+  }
+  if (summ != 0)
+    return summ;
+  else
+    return 0;//если ничего не найдено
+}
