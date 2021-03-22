36202c6b682d1bf50bc94d3f5e47f9a3c4e8bb82
MariaFokina <78910429+MariaFokina@users.noreply.github.com>
2021-03-22 14:04:24 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,20 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+   int left = 0, right = size - 1, count = 0; //границы массива
+   while (left < right) {
+      int middle = (left + right) / 2;
+      if (value > arr[middle])
+         left = middle + 1;
+      else
+         right = middle;
+   }
+   while (arr[left] == value) {
+      count++;
+      left++;
+   }
+   if (count != 0)
+      return count;
+   else
+      return 0; // если ничего не найдено
 }
